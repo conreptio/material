354bb81458486e0fcff4c8c37dd16674f9c547b5
conreptio <conreptio@gmail.com>
2017-05-13 19:29:35 +0200

    Add 'Menu' module

---
+++ bukkit/natrolite-utils/menu-legacy/src/main/java/org/natrolite/menu/legacy/Menu.java
@@ -0,0 +1,62 @@
+/*
+ * Natrolite - Minecraft Minigame Manager
+ *
+ * Copyright (c) 2017 XNITY <info@xnity.net>
+ * Copyright (c) 2017 Natrolite <info@natrolite.org>
+ * Copyright (c) 2017 Lukas Nehrke
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.natrolite.menu.legacy;
+
+import javax.annotation.Nullable;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.natrolite.menu.legacy.item.Icon;
+
+public interface Menu {
+
+  InventoryType getType();
+
+  int getSize();
+
+  @Nullable
+  String getTitle();
+
+  void setTitle(@Nullable String title);
+
+  Icon[] getIcons();
+
+  @Nullable
+  Icon getIcon(int slot) throws IllegalArgumentException;
+
+  void setIcon(int slot, @Nullable Icon icon) throws IllegalArgumentException;
+
+  void removeIcon(int slot);
+
+  void handleOpen(Player player);
+
+  void handleClose(Player player);
+
+  void update();
+
+  void update(Player player);
+
+  Inventory toInventory();
+
+  void onClick(int slot, MenuManager menuManager, Player player, InventoryClickEvent event);
+}
---
+++ bukkit/natrolite-utils/menu-legacy/src/main/java/org/natrolite/menu/legacy/item/DisplayIcon.java
@@ -0,0 +1,53 @@
+/*
+ * Natrolite - Minecraft Minigame Manager
+ *
+ * Copyright (c) 2017 XNITY <info@xnity.net>
+ * Copyright (c) 2017 Natrolite <info@natrolite.org>
+ * Copyright (c) 2017 Lukas Nehrke
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.natrolite.menu.legacy.item;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.natrolite.menu.legacy.MenuManager;
+
+public class DisplayIcon extends Icon {
+
+  public DisplayIcon(Material material) {
+    super(material);
+  }
+
+  public DisplayIcon(Material material, int amount) {
+    super(material, amount);
+  }
+
+  public DisplayIcon(Material material, int amount, byte durability) {
+    super(material, amount, durability);
+  }
+
+  public DisplayIcon(Material material, String name, String... lore) {
+    super(material, name, lore);
+  }
+
+  public DisplayIcon(Material material, int amount, byte durability, String title, String... lore) {
+    super(material, amount, durability, title, lore);
+  }
+
+  @Override
+  public void onClick(MenuManager menuManager, Player player, InventoryClickEvent event) {}
+}
