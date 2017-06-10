6c0ae97fdc1d350d742ac66791686d031af66ecd
conreptio <conreptio@gmail.com>
2017-06-10 19:43:29 +0200

    Add Command Module

---
+++ natrolite-utils/command/src/main/java/org/natrolite/command/error/SyntaxException.java
@@ -0,0 +1,39 @@
+/*
+ * Natrolite Minigames
+ *
+ * Copyright (c) 2017 XNITY <info@xnity.net>
+ * Copyright (c) 2017 Natrolite <info@natrolite.org>
+ * Copyright (c) 2017 Lukas Nehrke
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.natrolite.command.error;
+
+public class SyntaxException extends CommandException {
+
+  public SyntaxException() {}
+
+  public SyntaxException(String message) {
+    super(message);
+  }
+
+  public SyntaxException(String message, Throwable throwable) {
+    super(message, throwable);
+  }
+
+  public SyntaxException(Throwable throwable) {
+    super(throwable);
+  }
+}
