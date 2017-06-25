128d708dc10782c673efe05cd2e399a10ccf89b6
conreptio <conreptio@gmail.com>
2017-06-25 13:20:12 +0200

    Update License Header

--- natrolite-core/src/main/java/org/natrolite/impl/commands/ArenaCommand.java
+++ natrolite-core/src/main/java/org/natrolite/impl/commands/ArenaCommand.java
@@ -1,22 +1,20 @@
 /*
- * Natrolite Minigames
+ * This file is part of Natrolite.
  *
- * Copyright (c) 2017 XNITY <info@xnity.net>
- * Copyright (c) 2017 Natrolite <info@natrolite.org>
- * Copyright (c) 2017 Lukas Nehrke
+ * Copyright (C) 2016-2017 Lukas Nehrke
  *
- * This program is free software: you can redistribute it and/or modify
+ * Natrolite is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Natrolite is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with Natrolite. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.natrolite.impl.commands;
