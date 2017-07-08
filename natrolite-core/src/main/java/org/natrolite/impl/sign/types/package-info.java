25bfe0dd142f296fad65952ef68b6303dce07d3c
Lukas Nehrke <lukas.nehrke@hotmail.com>
2017-07-08 09:09:46 +0200

    Update Headers

--- natrolite-core/src/main/java/org/natrolite/impl/sign/types/package-info.java
+++ natrolite-core/src/main/java/org/natrolite/impl/sign/types/package-info.java
@@ -1,2 +1,21 @@
+/*
+ * This file is part of Natrolite.
+ *
+ * Copyright (C) 2016-2017 Lukas Nehrke
+ *
+ * Natrolite is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Natrolite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Natrolite. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 @org.natrolite.NonnullByDefault
 package org.natrolite.impl.sign.types;
