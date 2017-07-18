230c7c9d638f8103f2b018765c8b448854cf3cc1
kanoxx <conreptio@gmail.com>
2017-07-18 15:15:06 +0200

    Relocate services

---
+++ natrolite-api/src/main/java/org/natrolite/service/economy/transaction/TransactionTypes.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of SpongeAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.natrolite.service.economy.transaction;
+
+import org.natrolite.service.economy.Currency;
+import org.natrolite.service.economy.account.Account;
+
+public final class TransactionTypes {
+
+  /**
+   * Represents a transaction where an {@link Account} received some amount of a {@link Currency}.
+   */
+  public static final TransactionType DEPOSIT = new TransactionType("DEPOSIT");
+
+  /**
+   * Represents a transaction where an {@link Account} transferred some amount of a currency to another {@link Account}.
+   */
+  public static final TransactionType TRANSFER = new TransactionType("TRANSFER");
+
+  /**
+   * Represents a transaction where an {@link Account} lost some amount of a {@link Currency}.
+   */
+  public static final TransactionType WITHDRAW = new TransactionType("WITHDRAW");
+
+  private TransactionTypes() {}
+}
