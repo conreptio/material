505c1de6026c7411efb1b0ec9c8d96f884b5bf6a
Con Reptio <conreptio@gmail.com>
2017-12-16 16:25:58 +0100

    Update License

--- material-api/src/main/java/org/natrolite/service/NatroliteService.java
+++ material-api/src/main/java/org/natrolite/service/NatroliteService.java
@@ -1,20 +1,20 @@
 /*
- * This file is part of Natrolite.
+ * This file is part of Material.
  *
- * Copyright (C) 2016-2017 Lukas Nehrke
+ * Copyright (c) 2016-2017 Neolumia
  *
- * Natrolite is free software: you can redistribute it and/or modify
+ * Material is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Natrolite is distributed in the hope that it will be useful,
+ * Material is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Natrolite. If not, see <http://www.gnu.org/licenses/>.
+ * along with Material. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.natrolite.service;
